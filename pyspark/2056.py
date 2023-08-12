6337f1ff209e09caf3ab7ef7032fc702ecf6c45d
felipe <felipe_ludson@hotmail.com>
2023-08-12 17:02:25 -0300

    2056

--- pyspark/2056.py
+++ pyspark/2056.py
@@ -1,10 +1,9 @@
 from pyspark.sql import functions as F
-from pyspark.sql.types import StringType
 
 amazon_shipment
 
 amazon_shipment.groupBy(
     F.date_format('shipment_date', 'yyyy-MM').alias('month')
 ).agg(
-    F.countDistinct('shipment_id', 'sub_id').alias('cnt')
-).select(['month', 'cnt']).toPandas()
+    F.countDistinct('shipment_id', 'sub_id').alias('id')
+).select(['month', 'id']).toPandas()
